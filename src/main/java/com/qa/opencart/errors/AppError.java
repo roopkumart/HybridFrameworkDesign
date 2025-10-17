4652720b64cce32bb3245b6b1a97dd72336b1f4c
Roop kumar <troopkumar53@gmail.com>
2025-10-17 17:09:29 +0530

    this is opencart ui framework

---
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -0,0 +1,6 @@
+package com.qa.opencart.errors;
+
+public class AppError {
+public static final String INVALID_BROWSER_MESG="Please pass the right browser: only chrome/edge/safari/firefox is allowed";
+
+}
