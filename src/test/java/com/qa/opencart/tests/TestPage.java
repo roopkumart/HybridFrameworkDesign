85cdb4fafa97abd715ccc94cb8b3521a526f93c5
roopkumart <troopkumar53@gmail.com>
2026-05-08 18:50:51 +0530

    Testing class added

---
+++ src/test/java/com/qa/opencart/tests/TestPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPage {
+@Test
+public void footer() {
+	Assert.assertEquals(true, true);
+}
+}
